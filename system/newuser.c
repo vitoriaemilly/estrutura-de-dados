ed10d8ee09a22478e477b7acedf13b21ed1760bd
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-04-23 18:08:32 -0300

    new user atualizção

--- system/newuser.c
+++ system/newuser.c
@@ -6,9 +6,16 @@
 void new_user(){
     user u;
     FILE *f= fopen(USERS_FILE, "a");
-    if(f==NULL) printf("Erro no arquivo, tente de novo");
-    else {
-        fprint(f, "%s %s\n", u.login, u.password);
+    if(f==NULL) {
+        printf("Erro no arquivo, tente de novo");
+    }else {
+        printf("Digite o login que deseja:");
+        scanf(f, " %s ", u.login);
+         printf("Digite a sua senha:");
+        scanf(f, " %s ", u.password);
+
+         fprintf(f, " %s %s\n ", u.login, u.password);
+         fclose(f);
     }
 }
 
