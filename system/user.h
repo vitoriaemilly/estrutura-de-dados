5e0531ef5eacfcb3902e0e190d3dd05c902b1752
vitoria emilly <125896190+vitoriaemilly@users.noreply.github.com>
2026-04-23 17:40:26 -0300

    arquivo user-choice para escolha de login, cadastro ou senha arquivo user.h modificado com definição de uqsers_file

--- system/user.h
+++ system/user.h
@@ -1,10 +1,16 @@
 //Tad para  a implementação do usuario fixo
+#ifndef USER_H
+#define USER_H
+
 #define LOGIN_MAX 20
 #define PASSWORD_MAX 20
+#define USERS_FILE "users.txt"
 
 typedef struct user {
     char login[LOGIN_MAX];
     char password[PASSWORD_MAX];
 }user;
 
-void user_choice(user u);
+void choice_user(int *choice);
+
+#endif
